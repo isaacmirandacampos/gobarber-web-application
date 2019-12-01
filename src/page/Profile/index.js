5079db25fb131b1f3f3f5a6f7c6b62accfea9447
isaacmirandacampos <isaacdmcampos@outlook.com>
2019-12-01 16:28:38 -0300

    feature update profile

--- src/page/Profile/index.js
+++ src/page/Profile/index.js
@@ -1,7 +1,39 @@
 import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { Form, Input } from '@rocketseat/unform';
+import { updateProfileRequest } from '../../store/modules/user/actions';
 
 import { Container } from './styles';
 
 export default function Profile() {
-  return <Container />;
+  const dispatch = useDispatch();
+  const profile = useSelector(state => state.user.profile);
+  function handleSubmit(data) {
+    dispatch(updateProfileRequest(data));
+  }
+  return (
+    <Container>
+      <Form initialData={profile} onSubmit={handleSubmit}>
+        <Input name="name" placeholder="Nome completo" />
+        <Input name="email" type="email" placeholder="Seu endereco de email" />
+
+        <hr />
+
+        <Input
+          type="password"
+          name="OldPassword"
+          placeholder="Sua senha atual"
+        />
+        <Input type="password" name="password" placeholder="Nova senha" />
+        <Input
+          type="password"
+          name="confirmPassword"
+          placeholder="Confirme sua nova senha"
+        />
+        <button type="submit">Atualizar perfil</button>
+      </Form>
+
+      <button type="button">Sair do GoBarber</button>
+    </Container>
+  );
 }
